ae55c9c54f66159a1af4cad59f196ff06fc0703a
henriquevital00 <henriquevital1000@hotmail.com>
2020-12-12 15:38:55 -0300

    Renoved useless files

--- finalizacao/readme.md
+++
@@ -1,5 +0,0 @@
-###### **Projeto finalizado**
-
-Henrique Vital\
-João Vitor Dias  **22.119.006-9**\
-Laoni Felix
--- fisica/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from Equations import Equations
-
-if (__name__ == "__main__"):
-    obj=Equations()
-    while(True):
-        obj.message()
